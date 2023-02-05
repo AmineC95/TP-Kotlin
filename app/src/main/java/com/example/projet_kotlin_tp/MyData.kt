164b0c7e3c5b6d361bf99ee6489cfa4149c3a9a2
AmineC95 <amine.chioukh95@gmail.com>
2023-02-05 21:50:12 +0100

    commit

---
+++ app/src/main/java/com/example/projet_kotlin_tp/MyData.kt
@@ -0,0 +1,3 @@
+package com.example.projet_kotlin_tp
+
+class MyData (val name: String, val email: String)
---
+++ app/src/main/java/com/example/projet_kotlin_tp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,46 @@
+package com.example.projet_kotlin_tp.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.projet_kotlin_tp.databinding.FragmentGalleryBinding
+import com.example.projet_kotlin_tp.MyData
+
+class GalleryFragment : Fragment() {
+
+    private var _binding: FragmentGalleryBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val galleryViewModel =
+            ViewModelProvider(this).get(GalleryViewModel::class.java)
+
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val user = MyData("John Doe", "johndoe@example.com")
+        binding.user = user
+
+        val textView: TextView = binding.textGallery
+        galleryViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
